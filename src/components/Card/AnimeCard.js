cd0223e88d5f60230c888febe1174c4f10651eb9
Santiphap Watcharasukchit <iq.at.sk131@gmail.com>
2020-04-14 18:00:24 +0700

    [#3] Move season enum to separate enum file

--- src/components/Card/AnimeCard.js
+++ src/components/Card/AnimeCard.js
@@ -1,16 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
-const SeasonEnum = {
-    '1': 'Winter',
-    '2': 'Spring',
-    '3': 'Summer',
-    '4': 'Fall',
-    'Winter': '1',
-    'Spring': '2',
-    'Summer': '3',
-    'Fall': '4'
-};
+import { SeasonEnum } from '../../utils/enum'
 
 const AnimeCard = props => {
     const anime = props.anime;
---
+++ src/utils/enum.js
@@ -0,0 +1,10 @@
+export const SeasonEnum = {
+    '1': 'Winter',
+    '2': 'Spring',
+    '3': 'Summer',
+    '4': 'Fall',
+    'Winter': '1',
+    'Spring': '2',
+    'Summer': '3',
+    'Fall': '4'
+};
