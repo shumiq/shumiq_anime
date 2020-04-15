621b07303cd904e74cea8896d5e50b3f79d2507a
Santiphap Watcharasukchit <iq.at.sk131@gmail.com>
2020-04-15 21:16:28 +0700

    [#4] Show latest season anime as default

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,21 +3,27 @@ import './Home.css';
 import { onFirebaseDatabaseUpdate } from '../../utils/firebase';
 import { getLocalStorage } from '../../utils/localstorage';
 import AnimeCard from '../../components/Card/AnimeCard'
+import { AnimeFilter } from '../../components/Card/AnimeCard.filter'
 
 const Home = () => {
     const [database, setDatabase] = useState(JSON.parse(getLocalStorage('database')));
+    const [animelist, setAnimeList] = useState(database?.animelist);
+    const [currentPageAnimeList, setCurrentPageAnimeList] = useState(AnimeFilter(animelist));
 
     onFirebaseDatabaseUpdate(db => {
         setDatabase(db);
+        setAnimeList(db?.animelist);
     });
 
-    useEffect(() => { }, [database]);
+    useEffect(() => {
+        if (animelist) setCurrentPageAnimeList(AnimeFilter(animelist));
+    }, [animelist]);
 
     return (
         <div className="Home">
             <div className="container p-0 my-5">
                 <div className="row text-center w-100 m-0">
-                    {database?.animelist?.map(anime =>
+                    {currentPageAnimeList.map(anime =>
                         anime !== null &&
                         (< AnimeCard anime={anime} key={anime?.key} />)
                     )}
--- src/pages/Home/Home.test.js
+++ src/pages/Home/Home.test.js
@@ -7,9 +7,9 @@ import mockDatabase from '../../mock/database'
 jest.mock('../../utils/localstorage');
 
 describe('<Home />', () => {
-    it('should show correct anime card', () => {
+    it('should show latest season anime cards', () => {
         getLocalStorage.mockReturnValue(JSON.stringify(mockDatabase));
         const wrapper = shallow(<Home />);
-        expect(wrapper.find('AnimeCard')).toHaveLength(2);
+        expect(wrapper.find('AnimeCard')).toHaveLength(1);
     });
 });
