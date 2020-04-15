179ea254ed3adff3453bb39e8b90ab567c21d919
Santiphap Watcharasukchit <iq.at.sk131@gmail.com>
2020-04-15 21:18:21 +0700

    [#nc] remove home.css

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import './Home.css';
 import { onFirebaseDatabaseUpdate } from '../../utils/firebase';
 import { getLocalStorage } from '../../utils/localstorage';
 import AnimeCard from '../../components/Card/AnimeCard'
