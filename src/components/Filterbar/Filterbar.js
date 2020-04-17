2456c2c89666398cc83bad5a1fd296c3631e5495
Santiphap Watcharasukchit <iq.at.sk131@gmail.com>
2020-04-17 15:52:39 +0700

    [#5] Add static filter bar

--- src/components/Filterbar/Filterbar.js
+++ src/components/Filterbar/Filterbar.js
@@ -20,7 +20,7 @@ const Filterbar = props => {
 
     const gotoNextSeason = () => {
         if (hasNextSeason === 'invisible') return;
-        const nextSeasonIndex = Object.keys(seasonList).sort().findIndex(season => season === currentSeason)+1;
+        const nextSeasonIndex = Object.keys(seasonList).sort().findIndex(season => season === currentSeason) + 1;
         const nextSeason = Object.keys(seasonList).sort()[nextSeasonIndex];
         filter.season = nextSeason;
         setFilter(filter);
@@ -28,7 +28,7 @@ const Filterbar = props => {
 
     const gotoPreviousSeason = () => {
         if (havePreviousSeason === 'invisible') return;
-        const previousSeasonIndex = Object.keys(seasonList).sort().findIndex(season => season === currentSeason)-1;
+        const previousSeasonIndex = Object.keys(seasonList).sort().findIndex(season => season === currentSeason) - 1;
         const previousSeason = Object.keys(seasonList).sort()[previousSeasonIndex];
         filter.season = previousSeason;
         setFilter(filter);
@@ -38,6 +38,35 @@ const Filterbar = props => {
         <div className="Filterbar">
             <nav className="navbar navbar-expand-sm fixed-bottom p-0">
                 <div className="text-center w-100">
+                    <div className="collapse p-0 w-100 text-center" id="filterContent">
+                        <div className="container">
+                            <div className="row justify-content-around mx-2 bg-secondary shadow rounded w-100">
+                                <div className="col m-2" style={{ minWidth: '200px' }}>
+                                    <select className="form-control" id="filter-category">
+                                        <option selected value="1">All Anime</option>
+                                        <option value="2">Only Unseen</option>
+                                        <option value="3">Only Unfinished</option>
+                                        <option value="4">Only Finished</option>
+                                    </select>
+                                </div>
+                                <div className="col m-2 w-auto" style={{ minWidth: '200px' }}>
+                                    <select className="form-control" id="filter-season">
+
+                                    </select>
+                                </div>
+                                <div className="col m-2 w-auto" style={{ minWidth: '200px' }}>
+                                    <select className="form-control" id="filter-sort">
+                                        <option selected value="1">Sort by Season</option>
+                                        <option value="2">Sort by Score</option>
+                                    </select>
+                                </div>
+                                <div className="col m-2 w-auto" style={{ minWidth: '200px' }}>
+                                    <input type="text" className="form-control" id="filter-text" placeholder="Search..." />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                     <button className={"btn btn-secondary p-0 pt-1 mb-2 mr-2 mt-2 " + hasNextSeason} type="button" style={{ width: '48px', height: '48px' }} onClick={gotoNextSeason} id="next-season">
                         <i className="material-icons">keyboard_arrow_left</i>
                     </button>
