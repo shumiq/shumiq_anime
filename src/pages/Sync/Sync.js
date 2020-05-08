80ce8ea843d42cb8b9d8a8578dddaff587d9ee60
Santiphap Watcharasukchit <iq.at.sk131@gmail.com>
2020-05-08 15:15:40 +0700

    [#11] Add sync page

---
+++ src/pages/Sync/Sync.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sync = () => {
+    return (
+        <div className="Sync">
+            test
+        </div>
+    );
+};
+
+export default Sync;
--- src/utils/RouterConfig/routerConfig.js
+++ src/utils/RouterConfig/routerConfig.js
@@ -1,9 +1,15 @@
 import Anime from '../../pages/Anime/Anime';
+import Sync from '../../pages/Sync/Sync';
 
 export const ROUTER_CONFIG = [
     {
         path: '/',
         exact: true,
         component: Anime,
+    },
+    {
+        path: '/sync',
+        exact: true,
+        component: Sync,
     }
 ];
