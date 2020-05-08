6cb25d837279017c7db93f00b55c7d33bb3a946f
Santiphap Watcharasukchit <iq.at.sk131@gmail.com>
2020-05-08 15:39:03 +0700

    [#11] Add anime to table

--- src/pages/Sync/Sync.js
+++ src/pages/Sync/Sync.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getLocalStorage } from '../../utils/localstorage';
+import { onFirebaseDatabaseUpdate } from '../../utils/firebase';
 
 const Sync = () => {
+    const [database, setDatabase] = useState(getLocalStorage('database'));
+    const [animelist, setAnimeList] = useState(database?.animelist.filter(anime => anime != null));
+
+    onFirebaseDatabaseUpdate(db => {
+        setDatabase(db);
+        setAnimeList(db?.animelist.filter(anime => anime != null));
+    });
+
+    useEffect(() => { }, [animelist]);
+
+
     return (
         <div className="Sync">
-            test
+            <div className="container text-center" style={{ marginTop: '80px', marginBottom: '80px' }}>
+                <table className="table table-hover">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Title</th>
+                            <th>Download</th>
+                            <th>Sync</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {animelist.sort((a, b) => {
+                            if ((b.year * 10 + b.season % 10) - (a.year * 10 + a.season % 10) === 0)
+                                return a.title < b.title ? -1 : 1;
+                            else
+                                return (b.year * 10 + b.season % 10) - (a.year * 10 + a.season % 10);
+                        }).map(anime =>
+                            anime !== null &&
+                            <tr key={anime.key}>
+                                <td className="text-center align-middle">
+                                    <a href={anime.url} target="blank">
+                                        <img src={anime.cover_url} style={{ height: '50px' }} alt="cover" />
+                                    </a>
+                                </td>
+                                <td className="text-left align-middle">{anime.title}</td>
+                                <td className="text-center align-middle">{anime.download}</td>
+                                <td className="text-center align-middle"></td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
