f4a68222481d89f2e8f563466a0ca5123a7718da
Santiphap Watcharasukchit <iq.at.sk131@gmail.com>
2020-05-13 15:21:01 +0700

    [#24] Add router test

---
+++ src/utils/router.test.js
@@ -0,0 +1,36 @@
+import { IsAdmin } from './userDetail';
+import { getRouterConfig } from './router';
+
+
+jest.mock('./userDetail');
+jest.mock('./RouterConfig/routerConfig', () => ({
+    ROUTER_CONFIG: [
+        {
+            path: '/',
+            exact: true,
+            component: null,
+            auth: []
+        },
+        {
+            path: '/sync',
+            exact: true,
+            component: null,
+            auth: ['Admin']
+        }
+    ]
+}));
+
+describe('Router', () => {
+
+    it('should return all routers when admin', () => {
+        IsAdmin.mockReturnValue(true);
+        const routers = getRouterConfig()
+        expect(routers).toHaveLength(2);
+    });
+
+    it('should return only non-admin routers when not admin', () => {
+        IsAdmin.mockReturnValue(false);
+        const routers = getRouterConfig()
+        expect(routers).toHaveLength(1);
+    });
+});
