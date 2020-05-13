a7f194a525ee85435f7af8b88dd1defefd32f08e
Santiphap Watcharasukchit <iq.at.sk131@gmail.com>
2020-05-13 22:41:11 +0700

    [#24] Fix localstorage test

--- src/utils/localstorage.js
+++ src/utils/localstorage.js
@@ -4,16 +4,15 @@ export const setLocalStorage = (key, value) => {
     window.localStorage.setItem(`${STORAGE_KEY}_${key}`, JSON.stringify(value));
 };
 
-export function getLocalStorage(key) {
+export const getLocalStorage = key => {
     const value = window.localStorage.getItem(`${STORAGE_KEY}_${key}`);
 
     try {
         return JSON.parse(value) || {};
     } catch (err) {
-        console.error(err);
         return {};
     }
-}
+};
 
 export const removeLocalStorage = key => {
     window.localStorage.removeItem(`${STORAGE_KEY}_${key}`);
