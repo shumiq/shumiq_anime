993059a165dd7cb4ea47d6d803b37b14dfbc85dd
Santiphap Watcharasukchit <iq.at.sk131@gmail.com>
2020-05-17 13:55:44 +0700

    [#nc] Refactoring Anime card

--- src/components/Card/AnimeCard.js
+++ src/components/Card/AnimeCard.js
@@ -8,37 +8,48 @@ import AnimeInfoPopup from '../Popup/AnimeInfoPopup';
 import { getLocalStorage } from '../../utils/localstorage';
 
 const AnimeCard = props => {
-    const anime = props.anime;
-    const [editPopup, setEditPopup] = useState(false)
-    const [infoPopup, setInfoPopup] = useState(false)
-    const [animeInfo, setAnimeInfo] = useState(null)
+    const [state, setState] = useState({
+        anime: props.anime,
+        animeInfo: null,
+        popup: {
+            edit: false,
+            info: false
+        }
+    });
     const increase = field => {
-        let animeCopy = JSON.parse(JSON.stringify(anime));
+        let animeCopy = JSON.parse(JSON.stringify(state.anime));
         animeCopy[field] = parseInt(animeCopy[field]) + 1;
-        SaveAnime(anime.key, animeCopy);
+        SaveAnime(state.anime.key, animeCopy);
     }
+
     const showInfoPopup = async () => {
-        const response = await AnilistApi.getAnime(anime.title, anime.blacklist);
-        setAnimeInfo(response);
-        setInfoPopup(true);
+        const response = await AnilistApi.getAnime(state.anime.title, state.anime.blacklist);
+        setState({
+            ...state,
+            animeInfo: response,
+            popup: {
+                edit: false,
+                info: true
+            }
+        });
     }
 
     const layout = JSON.stringify(getLocalStorage('layout')) !== '{}' ? getLocalStorage('layout') : 'auto';
 
     return (
-        <div className={'anime-card p-3 ' + CardLayout[layout] }>
-            <div className={'card ' + (IsAdmin() && anime.view.toString() !== anime.download.toString() ? 'border border-primary' : '')} >
-                <div className="card-img-top " style={{ background: 'url(' + anime.cover_url + ') center / cover', height: '360px' }}>
+        <div className={'anime-card p-3 ' + CardLayout[layout]}>
+            <div className={'card ' + (IsAdmin() && state.anime.view.toString() !== state.anime.download.toString() ? 'border border-primary' : '')} >
+                <div className="card-img-top " style={{ background: 'url(' + state.anime.cover_url + ') center / cover', height: '360px' }}>
                     <small className="position-absolute align-middle px-1 pb-0 pt-0 text-white rounded" style={{ top: '5px', left: '5px', background: 'rgba(0,0,0,0.5)' }}>
                         <i className="material-icons" style={{ color: 'yellow', fontSize: '9px' }} >star</i>
-                        <b> {anime.score}</b>
+                        <b> {state.anime.score}</b>
                     </small>
                     <div className="position-absolute" style={{ top: '20px', right: '20px' }}>
                         <button className="btn btn-outline-light border-0 p-0 m-0" style={{ height: '24px' }}>
                             <i className="material-icons">share</i>
                         </button>
                         {IsAdmin() &&
-                            <button id='btn-edit' className="btn btn-outline-light border-0 p-0 m-0 ml-3" style={{ height: '24px' }} onClick={() => setEditPopup(true)}>
+                            <button id='btn-edit' className="btn btn-outline-light border-0 p-0 m-0 ml-3" style={{ height: '24px' }} onClick={() => setState({ ...state, popup: { info: false, edit: true } })}>
                                 <i className="material-icons">edit</i>
                             </button>
                         }
@@ -46,9 +57,9 @@ const AnimeCard = props => {
                 </div>
                 <div className="card-header position-absolute w-100 text-left p-2 h-auto pr-5"
                     style={{ background: 'rgba(0,0,0,0.5)', bottom: 'calc(100% - 360px)' }}>
-                    <p className="mb-0 mx-0 p-0 text-white text-left">{anime.title}</p>
+                    <p className="mb-0 mx-0 p-0 text-white text-left">{state.anime.title}</p>
                     <p className="mb-0" style={{ marginTop: '-5px', minHeight: '5px' }}>
-                        <small className="text-white-50">{anime.genres}</small>
+                        <small className="text-white-50">{state.anime.genres}</small>
                     </p>
                     <div className="position-absolute" style={{ top: 'calc(50% - 12px)', right: '10px' }}>
                         <button className="btn btn-outline-light border-0 p-0 m-0" style={{ height: '24px' }} id="btn-show-info" onClick={showInfoPopup}>
@@ -61,18 +72,18 @@ const AnimeCard = props => {
                         <tbody className="text-muted">
                             <tr>
                                 <td className="text-left px-3"><small>Studio</small></td>
-                                <td className="text-right px-3"><small>{anime.studio}</small></td>
+                                <td className="text-right px-3"><small>{state.anime.studio}</small></td>
                             </tr>
                             <tr>
                                 <td className="text-left px-3"><small>Season</small></td>
-                                <td className="text-right px-3"><small>{anime.year} {SeasonEnum[anime.season.toString()]}</small></td>
+                                <td className="text-right px-3"><small>{state.anime.year} {SeasonEnum[state.anime.season.toString()]}</small></td>
                             </tr>
                             {IsAdmin() &&
                                 <tr>
                                     <td className="text-left px-3"><small>View</small></td>
                                     <td className="text-right px-3">
-                                        <small>{anime.view}/{anime.download}</small>
-                                        {anime.view.toString() !== anime.download.toString() &&
+                                        <small>{state.anime.view}/{state.anime.download}</small>
+                                        {state.anime.view.toString() !== state.anime.download.toString() &&
                                             <b id='btn-add-view' className="text-primary" style={{ cursor: 'pointer' }} onClick={() => increase('view')}>+</b>
                                         }
                                     </td>
@@ -81,8 +92,8 @@ const AnimeCard = props => {
                             <tr>
                                 <td className="text-left px-3"><small>Download</small></td>
                                 <td className="text-right px-3">
-                                    <small>{anime.download}/{anime.all_episode}</small>
-                                    {IsAdmin() && anime.download.toString() !== anime.all_episode.toString() &&
+                                    <small>{state.anime.download}/{state.anime.all_episode}</small>
+                                    {IsAdmin() && state.anime.download.toString() !== state.anime.all_episode.toString() &&
                                         <b id='btn-add-download' className="text-primary" style={{ cursor: 'pointer' }} onClick={() => increase('download')}>+</b>
                                     }
                                 </td>
@@ -93,31 +104,31 @@ const AnimeCard = props => {
                 <div className="card-footer p-1">
                     <div className="d-flex justify-content-around w-auto">
 
-                        {!anime.gdriveid_public && <a id='btn-gdrive' className="btn btn-outline-secondary disabled h-auto border-0" type="button" href={'http://doc.google.com/drive/folders/' + anime.gdriveid_public} target='blank'>
+                        {!state.anime.gdriveid_public && <a id='btn-gdrive' className="btn btn-outline-secondary disabled h-auto border-0" type="button" href={'http://doc.google.com/drive/folders/' + state.anime.gdriveid_public} target='blank'>
                             <i className="material-icons align-middle">folder</i>
                         </a>}
-                        {anime.gdriveid_public && <a id='btn-gdrive' className="btn btn-outline-light h-auto border-0" role="button" href={'http://doc.google.com/drive/folders/' + anime.gdriveid_public} target='blank'>
+                        {state.anime.gdriveid_public && <a id='btn-gdrive' className="btn btn-outline-light h-auto border-0" role="button" href={'http://doc.google.com/drive/folders/' + state.anime.gdriveid_public} target='blank'>
                             <i className="material-icons align-middle">folder</i>
                         </a>}
 
-                        {anime.url === '' && <a id='btn-gphoto' className="btn btn-outline-secondary disabled h-auto border-0" type="button" href={anime.url} target='blank'>
+                        {state.anime.url === '' && <a id='btn-gphoto' className="btn btn-outline-secondary disabled h-auto border-0" type="button" href={state.anime.url} target='blank'>
                             <i className="material-icons align-middle">photo_library</i>
                         </a>}
-                        {anime.url !== '' && <a id='btn-gphoto' className="btn btn-outline-light h-auto border-0" role="button" href={anime.url} target='blank'>
+                        {state.anime.url !== '' && <a id='btn-gphoto' className="btn btn-outline-light h-auto border-0" role="button" href={state.anime.url} target='blank'>
                             <i className="material-icons align-middle">photo_library</i>
                         </a>}
 
-                        {IsAdmin() && anime.download_url === '' && <a id='btn-download' className="btn btn-outline-secondary disabled h-auto border-0" type="button" href={anime.download_url} target='blank'>
+                        {IsAdmin() && state.anime.download_url === '' && <a id='btn-download' className="btn btn-outline-secondary disabled h-auto border-0" type="button" href={state.anime.download_url} target='blank'>
                             <i className="material-icons align-middle">add_box</i>
                         </a>}
-                        {IsAdmin() && anime.download_url !== '' && <a id='btn-download' className="btn btn-outline-light h-auto border-0" role="button" href={anime.download_url} target='blank'>
+                        {IsAdmin() && state.anime.download_url !== '' && <a id='btn-download' className="btn btn-outline-light h-auto border-0" role="button" href={state.anime.download_url} target='blank'>
                             <i className="material-icons align-middle">add_box</i>
                         </a>}
                     </div>
                 </div>
             </div>
-            <EditAnimePopup anime={anime} show={editPopup} setShow={setEditPopup} />
-            <AnimeInfoPopup anime={anime} info={animeInfo} show={infoPopup} setShow={setInfoPopup} />
+            <EditAnimePopup anime={state.anime} show={state.popup.edit} setShow={show => setState({ ...state, popup: { info: false, edit: show } })} />
+            <AnimeInfoPopup anime={state.anime} info={state.animeInfo} show={state.popup.info} setShow={show => setState({ ...state, popup: { info: show, edit: false } })} />
         </div >
     );
 }
