51aca9a9ebd485bf542abd5bdbeefc614384d2ec
Santiphap Watcharasukchit <iq.at.sk131@gmail.com>
2020-05-22 15:56:41 +0700

    [#27] Add popup test in sync

--- src/pages/Sync/Sync.test.js
+++ src/pages/Sync/Sync.test.js
@@ -24,6 +24,44 @@ describe('<Sync />', () => {
     expect(wrapper.find('.row-anime')).toHaveLength(2);
   });
 
+  it('should call getAlbums api when click Load more...', async () => {
+    // Given
+    getLocalStorage.mockReturnValue(JSON.parse(JSON.stringify(mockDatabase)));
+    GooglePhotoApi.getAlbums.mockResolvedValue({
+      albums: [
+        { id: mockDatabase.animelist[0].gphotoid, mediaItemsCount: 2 },
+        { id: mockDatabase.animelist[1].gphotoid, mediaItemsCount: 2 },
+      ],
+    });
+    const wrapper = shallow(<Sync />);
+    // When
+    wrapper.find('#btn-load-more').simulate('click');
+    // Then
+    expect(wrapper.find('GeneralPopup')?.props()?.show).toBe(true);
+    await flushPromises();
+    expect(wrapper.find('GeneralPopup')?.props()?.show).toBe(false);
+    expect(GooglePhotoApi.getAlbums).toHaveBeenCalled();
+  });
+
+  it('should call getAllAlbums api when click Load all and show loading popup', async () => {
+    // Given
+    getLocalStorage.mockReturnValue(JSON.parse(JSON.stringify(mockDatabase)));
+    GooglePhotoApi.getAllAlbums.mockResolvedValue({
+      albums: [
+        { id: mockDatabase.animelist[0].gphotoid, mediaItemsCount: 2 },
+        { id: mockDatabase.animelist[1].gphotoid, mediaItemsCount: 2 },
+      ],
+    });
+    const wrapper = shallow(<Sync />);
+    // When
+    wrapper.find('#btn-load-all').simulate('click');
+    // Then
+    expect(wrapper.find('GeneralPopup')?.props()?.show).toBe(true);
+    await flushPromises();
+    expect(wrapper.find('GeneralPopup')?.props()?.show).toBe(false);
+    expect(GooglePhotoApi.getAllAlbums).toHaveBeenCalled();
+  });
+
   it('should show correct information & buttons after load from google photo', async () => {
     // Given
     getLocalStorage.mockReturnValue(JSON.parse(JSON.stringify(mockDatabase)));
@@ -44,7 +82,7 @@ describe('<Sync />', () => {
     expect(wrapper.find('.row-anime').at(1).text()).not.toContain('Update');
   });
 
-  it('should called save anime when update', async () => {
+  it('should called save anime when update and show loading popup', async () => {
     // Given
     getLocalStorage.mockReturnValue(JSON.parse(JSON.stringify(mockDatabase)));
     GooglePhotoApi.getAlbums.mockResolvedValue({
@@ -61,8 +99,10 @@ describe('<Sync />', () => {
     wrapper.find('#btn-load-more').simulate('click');
     await flushPromises();
     wrapper.find('.row-anime').at(0).find('#btn-update').simulate('click');
-    await flushPromises();
     // Then
+    expect(wrapper.find('GeneralPopup')?.props()?.show).toBe(true);
+    await flushPromises();
+    expect(wrapper.find('GeneralPopup')?.props()?.show).toBe(false);
     let updatedAnime = JSON.parse(JSON.stringify(mockDatabase.animelist[1]));
     updatedAnime.download = 2;
     updatedAnime.gdriveid_public = null;
