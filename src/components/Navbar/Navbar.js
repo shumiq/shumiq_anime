11632ce7693158bd462e542e4bf380256c77730b
Santiphap Watcharasukchit <iq.at.sk131@gmail.com>
2020-05-22 14:55:15 +0700

    [#27] Optimize navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,34 +3,35 @@ import history from '../../history';
 import { IsAdmin } from '../../utils/userdetail';
 import { onFirebaseAuthUpdate } from '../../utils/firebase';
 import { getLocalStorage, setLocalStorage } from '../../utils/localstorage';
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  const [state, setState] = useState({
-    isAnime:
-      history.location.pathname === '/' ||
-      history.location.pathname === '/sync',
-    isAdmin: IsAdmin(),
-    cardLayout:
-      JSON.stringify(getLocalStorage('layout')) !== '{}'
-        ? getLocalStorage('layout')
-        : 'auto',
-  });
+  const [isAnime, setIsAnime] = useState(
+    history.location.pathname === '/' || history.location.pathname === '/sync'
+  );
+  const [isAdmin, setIsAdmin] = useState(IsAdmin());
+  const [cardLayout, setCardLayout] = useState(
+    JSON.stringify(getLocalStorage('layout')) !== '{}'
+      ? getLocalStorage('layout')
+      : 'auto'
+  );
 
-  onFirebaseAuthUpdate(() => {
-    setState({ ...state, isAdmin: IsAdmin() });
-  });
+  useEffect(() => {
+    onFirebaseAuthUpdate(() => {
+      setIsAdmin(IsAdmin());
+    });
+  }, []);
 
-  const updateCardLayout = () => {
+  const updateCardLayout = useCallback(() => {
     let nextLayout = 'auto';
-    if (state.cardLayout === 'auto') nextLayout = 'small';
-    if (state.cardLayout === 'small') nextLayout = 'medium';
-    if (state.cardLayout === 'medium') nextLayout = 'large';
-    if (state.cardLayout === 'large') nextLayout = 'auto';
-    setState({ ...state, cardLayout: nextLayout });
+    if (cardLayout === 'auto') nextLayout = 'small';
+    if (cardLayout === 'small') nextLayout = 'medium';
+    if (cardLayout === 'medium') nextLayout = 'large';
+    if (cardLayout === 'large') nextLayout = 'auto';
+    setCardLayout(nextLayout);
     setLocalStorage('layout', nextLayout);
-  };
+  }, [cardLayout]);
 
   return (
     <div className="navbar">
@@ -56,18 +57,18 @@ const Navbar = () => {
                 id="link-anime"
                 className="nav-link btn"
                 to="/"
-                onClick={() => setState({ ...state, isAnime: true })}
+                onClick={() => setIsAnime(true)}
               >
                 Anime
               </Link>
             </li>
-            {state.isAnime && state.isAdmin && (
+            {isAnime && isAdmin && (
               <li className="nav-item">
                 <Link
                   id="link-sync"
                   className="nav-link btn"
                   to="/sync"
-                  onClick={() => setState({ ...state, isAnime: true })}
+                  onClick={() => setIsAnime(true)}
                 >
                   Sync Anime
                 </Link>
@@ -78,7 +79,7 @@ const Navbar = () => {
                 id="link-conan"
                 className="nav-link btn"
                 to="/conan"
-                onClick={() => setState({ ...state, isAnime: false })}
+                onClick={() => setIsAnime(false)}
               >
                 Conan
               </Link>
@@ -88,21 +89,21 @@ const Navbar = () => {
                 id="link-keyaki"
                 className="nav-link btn"
                 to="/keyaki"
-                onClick={() => setState({ ...state, isAnime: false })}
+                onClick={() => setIsAnime(false)}
               >
                 Keyakitte Kakenai
               </Link>
             </li>
           </ul>
           <ul className="navbar-nav text-center ml-auto">
-            {state.isAnime && (
+            {isAnime && (
               <li className="nav-item">
                 <p
                   id="btn-layout"
                   className="nav-link btn m-0"
                   onClick={updateCardLayout}
                 >
-                  {state.cardLayout}
+                  {cardLayout}
                 </p>
               </li>
             )}
