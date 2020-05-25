96d908935dc28b847bdf766329e6adb23abacaa0
Santiphap Watcharasukchit <iq.at.sk131@gmail.com>
2020-05-25 14:23:42 +0700

    [#22] Add search anime to addanimepopup

--- src/components/Popup/AddAnimePopup.js
+++ src/components/Popup/AddAnimePopup.js
@@ -1,10 +1,12 @@
 import { getLocalStorage } from '../../utils/localstorage';
 import { SaveAnime } from '../../utils/firebase';
+import AnilistApi from '../../api/anilist';
 import React, { useCallback, useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 
 const AddAnimePopup = (props) => {
   const [keyword, setKeyword] = useState('');
+  const [searchResult, setSearchResult] = useState([]);
   const closePopup = useCallback(() => props.setShow(false), [props]);
   const addAnime = useCallback(
     (anime) => {
@@ -14,6 +16,10 @@ const AddAnimePopup = (props) => {
     },
     [closePopup]
   );
+  const searchAnime = useCallback(async () => {
+    const result = await AnilistApi.searchAnime(keyword);
+    setSearchResult(result.slice(0, 5));
+  }, [keyword]);
   return (
     <div className="AddAnimePopup">
       <Modal
@@ -39,13 +45,41 @@ const AddAnimePopup = (props) => {
                   />
                 </td>
                 <td className="p-0 text-center">
-                  <button type="button" className="btn btn-primary">
+                  <button
+                    type="button"
+                    className="btn btn-primary"
+                    onClick={searchAnime}
+                  >
                     Search
                   </button>
                 </td>
               </tr>
             </tbody>
           </table>
+          {searchResult && searchResult.length > 0 && (
+            <table className="table m-0">
+              <tbody>
+                {searchResult.map(
+                  (anime) =>
+                    anime && (
+                      <tr key={anime.id} className="text-left">
+                        <td>
+                          <small>{anime.title.userPreferred}</small>
+                        </td>
+                        <td>
+                          <small>
+                            {anime.startDate.year} {anime.season}
+                          </small>
+                        </td>
+                        <td>
+                          <button className="btn btn-secondary">Add</button>
+                        </td>
+                      </tr>
+                    )
+                )}
+              </tbody>
+            </table>
+          )}
         </Modal.Body>
       </Modal>
     </div>
--- src/components/Popup/AddAnimePopup.test.js
+++ src/components/Popup/AddAnimePopup.test.js
@@ -1,8 +1,14 @@
+import AnilistApi from '../../api/anilist';
 import AddAnimePopup from './AddAnimePopup';
 import React from 'react';
 import { mount } from 'enzyme';
+import { act } from 'react-dom/test-utils';
+
+jest.mock('../../api/anilist');
 
 describe('<AddAnimePopup />', () => {
+  const flushPromises = () => new Promise(setImmediate);
+
   it('should not show when show props is false', () => {
     const wrapper = mount(<AddAnimePopup show={false} setShow={null} />);
     expect(wrapper.find('div.modal')).toHaveLength(0);
@@ -12,5 +18,62 @@ describe('<AddAnimePopup />', () => {
     const wrapper = mount(<AddAnimePopup show={true} setShow={null} />);
     expect(wrapper.find('div.modal').find('input')).toHaveLength(1);
     expect(wrapper.find('div.modal').find('button')).toHaveLength(1);
+  });
+
+  it('should call searchAnime from anilist api after click search', async () => {
+    const wrapper = mount(<AddAnimePopup show={true} setShow={null} />);
+    wrapper
+      .find('div.modal')
+      .find('input.form-control')
+      .simulate('change', { target: { value: 'test' } });
+    await act(async () => {
+      wrapper.find('div.modal').find('button').simulate('click');
+    });
+    expect(AnilistApi.searchAnime).toHaveBeenCalledWith('test');
+  });
+
+  it('should see search result after click search', async () => {
+    AnilistApi.searchAnime.mockResolvedValue([
+      {
+        id: 1,
+        title: { userPreferred: 'anime1' },
+        startDate: { year: 2020 },
+        season: 'SPRING',
+      },
+      {
+        id: 2,
+        title: { userPreferred: 'anime2' },
+        startDate: { year: 2019 },
+        season: 'WINTER',
+      },
+      {
+        id: 3,
+        title: { userPreferred: 'anime3' },
+        startDate: { year: 2018 },
+        season: 'SUMMER',
+      },
+    ]);
+    const wrapper = mount(<AddAnimePopup show={true} setShow={null} />);
+    await act(async () => {
+      wrapper.find('div.modal').find('button').simulate('click');
+    });
+    await flushPromises();
+    wrapper.update();
+    expect(wrapper.find('div.modal').find('table')).toHaveLength(2);
+    const result = wrapper
+      .find('div.modal')
+      .find('table')
+      .at(1)
+      .find('tbody')
+      .find('tr');
+    expect(result.at(0).text()).toContain('anime1');
+    expect(result.at(0).text()).toContain('2020');
+    expect(result.at(0).text()).toContain('SPRING');
+    expect(result.at(1).text()).toContain('anime2');
+    expect(result.at(1).text()).toContain('2019');
+    expect(result.at(1).text()).toContain('WINTER');
+    expect(result.at(2).text()).toContain('anime3');
+    expect(result.at(2).text()).toContain('2018');
+    expect(result.at(2).text()).toContain('SUMMER');
   });
 });
