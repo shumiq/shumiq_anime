beb926e7fe01d0e398ef848f4163dc7cd8f5ee85
IQ <santiphap.wat@gmail.com>
2021-08-09 18:03:50 +0700

    Video fullscreem

--- src/components/Dialog/VideoDialog.tsx
+++ src/components/Dialog/VideoDialog.tsx
@@ -118,10 +118,10 @@ export default function VideoDialog() {
 
 const useStyles = makeStyles((theme) => ({
   video: {
-    width: '90vw',
-    height: '50.625vw',
-    maxHeight: '90vh',
-    maxWidth: '160vh',
+    width: '100vw',
+    height: '56.25vw',
+    maxHeight: '100vh',
+    maxWidth: '177vh',
     margin: 'auto',
   },
   customControl: {
