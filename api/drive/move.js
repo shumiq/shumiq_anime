7f1f34d2a9cd6796e4a5e3116b4512e57b49acee
IQ <iq.at.sk131@gmail.com>
2021-12-07 13:10:51 +0700

    Lint api

--- api/drive/move.js
+++ api/drive/move.js
@@ -3,17 +3,17 @@ const axios = require('axios');
 const router = express.Router();
 
 const moveUrl =
-    'https://shumiq.synology.me:5001/webapi/entry.cgi?api=SYNO.FileStation.CopyMove&version=3&method=start&path=__FROM__&dest_folder_path=__TO__&remove_src=true';
+  'https://shumiq.synology.me:5001/webapi/entry.cgi?api=SYNO.FileStation.CopyMove&version=3&method=start&path=__FROM__&dest_folder_path=__TO__&remove_src=true';
 
 router.get('/', async (req, res) => {
-    const sid = req.query.sid.toString();
-    const from = encodeURIComponent(req.query.from.toString());
-    const to = encodeURIComponent(req.query.to.toString());
-    const moveRes = await axios.get(
-        moveUrl.replace("__FROM__",from).replace("__TO__",to) + `&_sid=${sid}`
-    );
-    if(moveRes.data.success) res.status(200).send();
-    res.status(400).send();
+  const sid = req.query.sid.toString();
+  const from = encodeURIComponent(req.query.from.toString());
+  const to = encodeURIComponent(req.query.to.toString());
+  const moveRes = await axios.get(
+    moveUrl.replace('__FROM__', from).replace('__TO__', to) + `&_sid=${sid}`
+  );
+  if (moveRes.data.success) res.status(200).send();
+  res.status(400).send();
 });
 
 module.exports = router;
--- api/drive/signin.js
+++ api/drive/signin.js
@@ -5,22 +5,27 @@ const router = express.Router();
 const admin = require('../utils/firebase');
 
 const signInUrl =
-    'https://shumiq.synology.me:5001/webapi/auth.cgi?api=SYNO.API.Auth&version=3&method=login&account=__USERNAME__&passwd=__PASSWORD__&session=FileStation&format=sid';
+  'https://shumiq.synology.me:5001/webapi/auth.cgi?api=SYNO.API.Auth&version=3&method=login&account=__USERNAME__&passwd=__PASSWORD__&session=FileStation&format=sid';
 
 router.get('/', async (req, res) => {
-  const token = req.query.token?.toString() || "";
-  const currentUser = token === "" ? null : await admin.auth().verifyIdToken(token);
-  const adminUID = process.env.FIREBASE_ADMIN_UID || "undefined";
-  let username = process.env.SYNOLOGY_API_GUEST_USERNAME || "undefined";
-  let password = process.env.SYNOLOGY_API_GUEST_PASSWORD || "undefined";
-  let role="guest";
-  if(adminUID === currentUser?.uid) {
+  const token = req.query.token?.toString() || '';
+  const currentUser =
+    token === '' ? null : await admin.auth().verifyIdToken(token);
+  const adminUID = process.env.FIREBASE_ADMIN_UID || 'undefined';
+  let username = process.env.SYNOLOGY_API_GUEST_USERNAME || 'undefined';
+  let password = process.env.SYNOLOGY_API_GUEST_PASSWORD || 'undefined';
+  let role = 'guest';
+  if (adminUID === currentUser?.uid) {
     username = process.env.SYNOLOGY_API_ADMIN_USERNAME || username;
     password = process.env.SYNOLOGY_API_ADMIN_PASSWORD || password;
-    role="admin";
+    role = 'admin';
   }
-  const signInRes = await axios.get(signInUrl.replace("__USERNAME__",username).replace("__PASSWORD__",password));
-  res.send({role: role, ...signInRes.data});
+  const signInRes = await axios.get(
+    signInUrl
+      .replace('__USERNAME__', username)
+      .replace('__PASSWORD__', password)
+  );
+  res.send({ role: role, ...signInRes.data });
 });
 
 module.exports = router;
